2c99174214e296a7d06a08cda485becd29398c35
Ilya Dutsev <elijah.dutsev@gmail.com>
2019-12-14 16:42:11 +0300

    initial commit

---
+++ BlockBreaker/Assets/CameraSizeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSizeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Camera>().orthographicSize = 6;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
